3fa7b5324fdbfa895118d4d4042e47ce20193575
Sameer Alam <sameeralam57k@gmail.com>
2025-08-06 00:00:18 +0530

    improved UI/UX login/signup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,48 +1,128 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Input from "../components/Input";
+import { Eye, EyeOff } from "lucide-react";
+import Logo from "../components/Logo";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import toast, { Toaster } from "react-hot-toast";
 
-const BACKEND_URL = import.meta.env.BACKEND_URL || "https://bookhub-1-ijt4.onrender.com";
+const BACKEND_URL =
+  import.meta.env.BACKEND_URL || "https://bookhub-1-ijt4.onrender.com";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const [hidePass, setHidePass] = useState(true);
   const { login } = useAuth();
   const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+  const handleLogin = async (data) => {
     try {
       const res = await axios.post(`${BACKEND_URL}/api/auth/login`, {
-        email,
-        password,
+        email: data.email,
+        password: data.password,
       });
       login(res.data);
-      alert("Login successful");
+      toast.success("Login successful");
       navigate("/");
     } catch (err) {
-      alert(err.response?.data?.error || "Login failed");
+      toast.error(err.response?.data?.error || "Login failed");
     }
   };
 
   return (
-    <div className="bg-orange-100 min-h-screen grid grid-cols-2 justify-center items-center">
-    <div className="p-6 max-w-sm mx-auto bg-[#BB6653] min-h-[400px] rounded-lg shadow-lg">
-      <form onSubmit={handleLogin} className="space-y-4">
-        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}
-          className="w-full p-2 border rounded" required />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}
-          className="w-full p-2 border rounded" required />
-        <button type="submit" className="w-full bg-orange-300 text-black py-2 rounded">Login</button>
-      </form>
-    </div>
-
-    <div className="max-w-xl mx-auto">
-        <img src="https://user-gen-media-assets.s3.amazonaws.com/gpt4o_images/b4ceb184-f02d-432e-81b5-d4f14439e975.png" alt=""
-            className="w-full h-auto rounded-lg shadow-lg"
-        />
-    </div>
+    <div className="bg-orange-100 h-[calc(100vh-74px)] w-full flex justify-center items-center">
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="flex max-w-full bg-[#BB6653] rounded-lg shadow-xl ">
+        <div className="flex flex-col">
+          <div className="p-6 mx-[5rem] max-w-md bg-[#BB6653] h-fit ">
+            <div className="w-[20rem] pt-10">
+              <Logo
+                clss={"my-6 "}
+                classname={"font-extrabold text-blue-800 text-2xl"}
+              />
+              <span className="text-center block font-bold text-3xl">
+                Wellcome Back !
+              </span>
+              <p className="text-xs m-1">
+                Enter to get unlimited access to knowledge & information
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className=" w-96">
+              <form onSubmit={handleSubmit(handleLogin)} className="space-y-6 ">
+                <div>
+                  <div>
+                    <Input
+                      label={"Email*"}
+                      id={"email"}
+                      placeholder={"Enter your email addess"}
+                      {...register("email", { required: true })}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">Password*</label>
+                    <div className="flex items-center bg-[#E8F0FE] rounded">
+                      <Input
+                        type={hidePass ? "password" : "text"}
+                        className={"outline-none "}
+                        id={"password"}
+                        placeholder={"Enter password"}
+                        {...register("pasword", { required: true })}
+                      />
+                      {hidePass ? (
+                        <EyeOff
+                          size={18}
+                          color="gray"
+                          className="cursor-pointer m-2"
+                          onClick={() => setHidePass((prev) => !prev)}
+                        />
+                      ) : (
+                        <Eye
+                          size={18}
+                          color="gray"
+                          className="cursor-pointer m-2"
+                          onClick={() => setHidePass((prev) => !prev)}
+                        />
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className={`w-full bg-orange-300 ${
+                    isSubmitting && "opacity-75"
+                  } text-black py-2 rounded`}
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? "Submiting..." : "Submit"}
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="text-center mb-20 mt-6">
+            <span>Don't have account ? </span>
+            <Link to={"/signup"} className="underline text-blue-800">
+              register here
+            </Link>
+          </div>
+        </div>
+        <div className="max-w-xl mx-auto">
+          <img
+            src="../..//books.png"
+            alt=""
+            className="max-w-[27.5rem] h-auto rounded-r-xl"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,44 +1,130 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
-const BACKEND_URL = import.meta.env.BACKEND_URL || "https://bookhub-1-ijt4.onrender.com";
+import Logo from "../components/Logo";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Eye, EyeOff } from "lucide-react";
+import toast, { Toaster } from "react-hot-toast";
+import Input from "../components/Input";
+const BACKEND_URL =
+  import.meta.env.BACKEND_URL || "https://bookhub-1-ijt4.onrender.com";
 
 const Signup = () => {
-  const [form, setForm] = useState({ name: "", email: "", password: "" });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const [hidePass, setHidePass] = useState(true);
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSingUp = async (data) => {
     try {
-      await axios.post(`${BACKEND_URL}/api/auth/signup`, form);
-      alert("Signup successful! You can now login.");
+      await axios.post(`${BACKEND_URL}/api/auth/signup`, data);
+      toast.success("Signup successful! You can now login.");
       navigate("/login");
     } catch (err) {
-      alert(err.response?.data?.error || "Signup failed");
+      toast.error(err.response?.data?.error || "Signup failed");
     }
   };
 
   return (
-    <div className="bg-orange-100 min-h-screen grid grid-cols-2 justify-center items-center">
-    <div className="p-6 max-w-sm mx-auto bg-[#BB6653] min-h-[400px] rounded-lg shadow-lg">
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <input type="text" placeholder="Name" required className="w-full p-2 border rounded"
-          value={form.name} onChange={(e) => setForm({ ...form, name: e.target.value })} />
-        <input type="email" placeholder="Email" required className="w-full p-2 border rounded"
-          value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} />
-        <input type="password" placeholder="Password" required className="w-full p-2 border rounded"
-          value={form.password} onChange={(e) => setForm({ ...form, password: e.target.value })} />
-        <button type="submit" className="w-full bg-orange-300 text-black py-2 rounded">Signup</button>
-      </form>
-    </div>
+    <div className="bg-orange-100 h-[calc(100vh-74px)] w-full flex justify-center items-center ">
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="flex max-w-full bg-[#BB6653] rounded-lg  shadow-xl ">
+        <div className="flex flex-col">
+          <div className="p-6 mx-[5rem] max-w-md bg-[#BB6653] h-full ">
+            <div className="w-[20rem] pt-10">
+              <Logo
+                clss={"mb-6"}
+                classname={"font-extrabold text-blue-800 text-2xl"}
+              />
+              <span className="text-center block font-bold text-3xl">
+                Wellcome Back !
+              </span>
+              <p className="text-xs m-1">
+                Enter to get unlimited access to knowledge & information
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className=" w-96">
+              <form onSubmit={handleSubmit(handleSingUp)}>
+                <div>
+                  <Input
+                    label={"Name*"}
+                    type="text"
+                    placeholder="Name"
+                    className="w-full p-2 border rounded bg-[#E8F0FE]"
+                    {...register("name", { required: true })}
+                  />
+                </div>
 
-    <div className="max-w-xl mx-auto">
-        <img src="https://user-gen-media-assets.s3.amazonaws.com/gpt4o_images/b4ceb184-f02d-432e-81b5-d4f14439e975.png" alt=""
-            className="w-full h-auto rounded-lg shadow-lg"
-        />
+                <div>
+                  <Input
+                    label={"Email*"}
+                    type="email"
+                    placeholder="Email"
+                    className="w-full p-2 border rounded bg-[#E8F0FE]"
+                    {...register("email", { required: true })}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password">Password*</label>
+                  <div className="flex items-center bg-[#E8F0FE] rounded">
+                    <Input
+                      type={hidePass ? "password" : "text"}
+                      className={"outline-none "}
+                      id="password"
+                      placeholder={"Enter password"}
+                      {...register("pasword", { required: true })}
+                    />
+                    {hidePass ? (
+                      <EyeOff
+                        size={18}
+                        color="gray"
+                        className="cursor-pointer m-2"
+                        onClick={() => setHidePass((prev) => !prev)}
+                      />
+                    ) : (
+                      <Eye
+                        size={18}
+                        color="gray"
+                        className="cursor-pointer m-2"
+                        onClick={() => setHidePass((prev) => !prev)}
+                      />
+                    )}
+                  </div>
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full mt-6 bg-orange-300 text-black py-2 rounded"
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? "Loading..." : "Signup"}
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="text-center mb-16 mt-6">
+            <span>Already have an account ? </span>
+            <Link to={"/login"} className="underline text-blue-800">
+              Login 
+            </Link>
+          </div>
+        </div>
+        <div className="max-w-xl mx-auto">
+          <img
+            src="../..//books.png"
+            alt=""
+            className="max-w-[27.5rem] h-auto rounded-r-xl"
+          />
+        </div>
+      </div>
     </div>
-  </div>
   );
 };
 
