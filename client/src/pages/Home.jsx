442b170203123dd0448acda12a8018bdca869404
Anmol Sah <kumarxxx242424@gmail.com>
2025-08-05 18:53:36 +0530

    addded toast message for login, signup

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,8 +4,10 @@ import BookCard from "../components/BookCard";
 import BookModal from "../components/BookModal";
 import AddBookForm from "../components/AddBookForm";
 import { useAuth } from "../context/AuthContext";
+import toast from "react-hot-toast";
 
-const BACKEND_URL = import.meta.env.BACKEND_URL || "https://bookhub-1-ijt4.onrender.com";
+const BACKEND_URL =
+  import.meta.env.BACKEND_URL || "https://bookhub-1-ijt4.onrender.com";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
@@ -13,9 +15,10 @@ const Home = () => {
   const { user } = useAuth();
 
   const fetchBooks = () => {
-    axios.get(`${BACKEND_URL}/api/books`)
+    axios
+      .get(`${BACKEND_URL}/api/books`)
       .then((res) => setBooks(res.data))
-      .catch((err) => alert("Failed to load books"));
+      .catch((err) => toast.error("Failed to load books"));
   };
 
   useEffect(() => {
@@ -24,17 +27,25 @@ const Home = () => {
 
   return (
     <div className="p-4 bg-orange-100 min-h-screen">
-      <p className="text-center mb-4 text-xl animate-bounce">Login/Signup to add books</p>
+      <p className="text-center mb-4 text-xl animate-bounce">
+        Login/Signup to add books
+      </p>
       {user && <AddBookForm onAdded={fetchBooks} />}
       {selected && (
         <BookModal book={selected} onClose={() => setSelected(null)} />
       )}
 
       <div>
-        <h2 className="text-6xl mb-5 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-black to-black text-center mt-6">All Books</h2>
+        <h2 className="text-6xl mb-5 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-black to-black text-center mt-6">
+          All Books
+        </h2>
         <div className="grid grid-cols-1 mt-4 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {books.map((book) => (
-            <BookCard key={book._id} book={book} onClick={() => setSelected(book)} />
+            <BookCard
+              key={book._id}
+              book={book}
+              onClick={() => setSelected(book)}
+            />
           ))}
         </div>
       </div>
